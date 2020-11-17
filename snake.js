b128421cb7f36f27682306c07e1edd47cfb265b9
Blend-77 <redsenpaiyt@gmail.com>
2020-11-16 22:07:37 -0800

    Update snake.js

--- snake.js
+++ snake.js
@@ -2,10 +2,40 @@ class Snake {
   constructor() {
     this.body = [];
     this.body[0] = createVector(0,0)
+    this.xdir = 0;
+    this.L = 1;
+    this.ydir = 0;
+  }
+
+  setDir(x, y) {
+    this.xdir = x;
+    this.ydir = y;
+  }
+
+  grow() {
+    this.len++;
+    this.body.push(createVector());
+  }
+
+  eat(pos) {
+    let x = this.body[0].x;
+    let y = this.body[0].y;
+    if (x == pos.x && y == pos.y) {
+      print("ATE FOOD");
+      this.grow();
+      return true;
+    }
+    return false;
   }
 
   update() {
     this.body[0].x += this.xdir;
     this.body[0].y += this.ydir;
+  }
+
+  show() {
+    noStroke();
+    fill(255,255,255);
+    rect(this.body[0].x, this.body[0].y, 1,1);
   }
 }
