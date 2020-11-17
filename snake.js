3ce2c6a6da5aeb53d37931c8b7c993a146b15ca5
Blend-77 <redsenpaiyt@gmail.com>
2020-11-17 09:31:19 -0800

    Update snake.js

--- snake.js
+++ snake.js
@@ -13,8 +13,8 @@ class Snake {
   }
 
   grow() {
-    this.len++;
-    this.body.push(createVector());
+    this.L++;
+    this.body.push(createVector(0,0));
   }
 
   eat(pos) {
@@ -28,14 +28,49 @@ class Snake {
     return false;
   }
 
+  EndGame() {
+    background(255,0,0);
+    noLoop();
+  }
+
+  checkLocation() {
+    //nothing
+    for (let i = -0; i < this.L; i++) {
+      if (i != 0) {
+        if (this.body[0].x == this.body[i].x && this.body[0].y == this.body[i].y) {
+          this.EndGame()
+        }
+      }
+    }
+  }
+
   update() {
+    let prevLX = this.body[0].x;
+    let prevLY = this.body[0].y;
+    let tempx;
+    let tempy;
     this.body[0].x += this.xdir;
     this.body[0].y += this.ydir;
+    this.body[0].x = constrain(this.body[0].x, 0, w-1);
+    this.body[0].y = constrain(this.body[0].y, 0, h-1);
+    for (let i = 0; i < this.L; i++) {
+      if (i != 0) {
+        tempx = this.body[i].x;
+        tempy = this.body[i].y;
+        this.body[i].x = prevLX;
+        this.body[i].y = prevLY;
+        prevLX = tempx;
+        prevLY = tempy;
+      }
+    }
+    this.checkLocation();
   }
 
   show() {
     noStroke();
     fill(255,255,255);
-    rect(this.body[0].x, this.body[0].y, 1,1);
+    for (let i = 0; i < this.L; i++) {
+      rect(this.body[i].x, this.body[i].y, 1,1)
+    }
   }
 }
